d886ba78c2d1bd45891347c4aec91ca9535980a7
tirdeaalex <sanydge@gmail.com>
2020-03-26 17:08:08 +0200

    Detecting Webcam objects

--- motion_detector.py
+++ motion_detector.py
@@ -1,26 +1,43 @@
 import cv2
-import time
+
+first_frame = None
 
 video = cv2.VideoCapture(1)
 
-a = 0
-
 while True:
-    a = a + 1
     check, frame = video.read()
 
-    print(check)
-    print(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # time.sleep(3)
-    cv2.imshow("Capturing", frame)
+    if first_frame is None:
+        first_frame = gray
+        continue
+
+    delta_frame = cv2.absdiff(first_frame, gray)
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Gray Frame", gray)
+    cv2.imshow("Delta Frame", delta_frame)
+    cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
     key = cv2.waitKey(1)
+    print(gray)
+    print(delta_frame)
 
     if key == ord('q'):
         break
 
-print(a)
+
 video.release()
 cv2.destroyAllWindows()
